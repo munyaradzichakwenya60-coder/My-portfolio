dfce024e60efa4ada86e83139fb662ecbd320d9c
munyaradzichakwenya60-coder <munyaradzichakwenya60@gmail.com>
2026-04-13 15:09:44 +0200

    first commit

---
+++ exp.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    
+    void Start()
+    {
+        // PUT THE CODE RIGHT HERE:
+        GameObject.CreatePrimitive(PrimitiveType.Cube);
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
